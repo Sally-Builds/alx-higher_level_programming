13b15884dc838f95789f3c4596ccc1641d458db4
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-06 16:38:09 -0700

    alphabet reverse alternate upper and lowercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,7 +4,4 @@ a = ord('a')
 z = ord('z')
 
 for i in range(z, a-1, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
