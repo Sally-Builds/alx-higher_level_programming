ccadd450623b52bc11fdbf462087093205c89513
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-05 21:01:05 -0700

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for i in str:
+        j = i;
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            j = chr(ord(i) - 32)
+        print(j, end="")
+    print()
