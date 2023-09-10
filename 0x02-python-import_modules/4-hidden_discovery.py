4e0fb02a8de2f0ed281277e2f346d8bd13189630
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-10 08:35:37 -0700

    who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
 if __name__ == "__main__":
-    import hidden_4 
-    print(hidden_4.my_secret_santa.__name__)
-    print(hidden_4.print_hidden.__name__)
-    print(hidden_4.print_school.__name__)
+    import hidden_4
+    for op in dir(hidden_4):
+        if op[:2] != "__":
+            print(op)
