dc9f725d3d82f47c8bbef3b39837b94168098499
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-10 08:16:14 -0700

    who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    from hidden_4 import my_secret_santa, print_hidden, print_school
+    print(my_secret_santa.__name__)
+    print(print_hidden.__name__)
+    print(print_school.__name__)
