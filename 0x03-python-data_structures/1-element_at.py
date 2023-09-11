5c2d9ffc08fdad586d45dd5cb4113c067fad49d7
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-11 04:32:37 -0700

    debug

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        if idx < 0:
-            return none
-        elif idx >= len(my_list):
-            return none
-        else:
-            return my_list[idx]
+def element_at(my_list, idx):
+    if idx < 0:
+        return none
+    elif idx >= len(my_list):
+        return none
+    else:
+        return my_list[idx]
