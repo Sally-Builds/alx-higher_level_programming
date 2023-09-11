9c14b4e91c24915bb181893169ca4655eda95281
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-11 05:17:15 -0700

    debugging new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,5 @@ def new_in_list(my_list, idx, element):
     if my_list:
         for i in my_list:
             new_list.append(i)
-    new_list[idx] = element
+        new_list[idx] = element
     return new_list
