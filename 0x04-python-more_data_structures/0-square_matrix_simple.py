171642df604a0f926ba785b0cd942a96ed25a7d9
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-13 12:24:49 -0700

    debugging

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,8 @@ def square_matrix_simple(matrix=[]):
     matrix_square = []
 
     if len(matrix) > 0:
-        row = []
         for i in range(len(matrix)):
+            row = []
             for j in range(len(matrix[i])):
                 row.append(matrix[i][j] ** 2)
             matrix_square.append(row)
