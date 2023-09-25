bf24fd2a0bfd064e962f710c0c0cdd77a1119faf
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-25 12:02:22 -0700

    debugging

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         print()
         return count
-    except IndexError as e:
+    except (ValueError, IndexError)
         print(e)
     return count
