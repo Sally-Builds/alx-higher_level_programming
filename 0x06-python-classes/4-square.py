783dfbe85a9f0bd524effc813a372a89623f55a1
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-26 15:31:06 -0700

    fn overload

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A module that defines a square """
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """ Area of the square"""
+        return self.__size * self.__size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        """size setter
+       Args:
+            vale: new size value
+       """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
