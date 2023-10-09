3b8ea3b77833d99b19201c44c8fde76c707d3f76
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-10-09 06:25:31 -0700

    dir object lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module - get avaialable attribute and methods of an object """
+
+
+def lookup(obj):
+    """ Returns the attributes and methods of an objet"""
+    return dir(obj)
