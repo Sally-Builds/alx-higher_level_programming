f4e09c2a102a18af9f12ce5b3cd70e410aaab3d1
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-10-10 12:55:33 -0700

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module: Reads the content of a file """
+import json
+
+def to_json_string(my_obj):
+    """ write to a file and return the number of characters written """
+    return json.dumps(my_obj)
