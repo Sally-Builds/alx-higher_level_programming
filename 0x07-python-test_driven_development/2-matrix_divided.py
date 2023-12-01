c251a790f770c0a75547bf47e178ecb8100edf53
root <uzoagulujoshua@yahoo.com>
2023-12-01 04:14:54 -0800

    styling

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,9 @@
 This module defines a matrix division function
 """
 
+
 def matrix_divided(matrix, div):
+
     """This function divides all elements of a matrix by a given number
     Args:
         matrix: A list of lists (matrix)- members can be of type ints or floats
@@ -17,14 +19,19 @@ def matrix_divided(matrix, div):
         A new matrix which represents the result of the divisions
     """
     if not isinstance(matrix, list) or matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/float")
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/float")
-    
-    if not all((isinstance(row, int) or isinstance(row, float)) for item in matrix for row in item):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/float")
-    
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all((isinstance(row, int) or isinstance(row, float))
+               for item in matrix
+               for row in item):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
     if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
 
