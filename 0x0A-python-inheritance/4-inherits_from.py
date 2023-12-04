42d09d7d5a7909078264f502fd59016919a8d866
root <uzoagulujoshua@yahoo.com>
2023-12-04 14:17:21 -0800

    debug

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Module: checks if object is inherits from class"""
+
 
 def inherits_from(obj, a_class):
+    """returns True if object is subclass"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
