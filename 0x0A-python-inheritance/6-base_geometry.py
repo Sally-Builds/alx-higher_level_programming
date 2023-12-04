a03ba38c32cc1f828406d4912b142b602cc37643
root <uzoagulujoshua@yahoo.com>
2023-12-04 14:33:29 -0800

    base geo extended

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module:  Base Geometry class"""
+
+
+class BaseGeometry:
+    """ Base geometry class"""
+    def area(self):
+        """Area of geometry """
+        raise Exception("area() is not implemented")
