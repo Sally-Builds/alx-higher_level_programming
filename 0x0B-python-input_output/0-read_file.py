fff965e8a63dbc275df4b9ab4643e6fe9f297c5c
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-12-05 20:02:23 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module: Reads the content of a file """
+
+
+def read_file(filename=""):
+	""" reads a file """
+	with open(filename, 'r') as file:
+		for line in file:
+			print(line);
