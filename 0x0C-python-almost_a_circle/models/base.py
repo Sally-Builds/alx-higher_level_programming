01980f0ffac244a8fbd48d4d44593fedcc558871
Uzoagulu Joshua <uzoagulujoshua@yahoo.com>
2023-12-11 20:40:45 -0800

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,8 +23,23 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """dictionary to json"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """json string to dictionary"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """ save to file"""
+        with open(cls.__name__ + '.json', 'w') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
